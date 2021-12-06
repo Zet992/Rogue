c0db1ee52918fddba4703181d178c8a2475d5c10
Zet992 <rodionovalexey11@mail.ru>
2021-12-06 14:49:06 +0300

    feat: add shooting

--- entities.py
+++ entities.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 
@@ -26,6 +28,14 @@ class Player(Entity):
                                                 self.width,
                                                 self.height))
 
+    def shot(self):
+        mx, my = pygame.mouse.get_pos()
+        start_pos = (self.x + self.width // 2, self.y + self.height // 2)
+        rot = math.degrees(math.atan2(my - start_pos[0], mx - start_pos[1]))
+        rot = math.radians(rot)
+        move = math.cos(rot) * 10, math.sin(rot) * 10
+        bullet = Bullet(start_pos[0], start_pos[1], 1, 1, move=move)
+        return bullet
 
 class Enemy(Entity):
     pass
@@ -37,3 +47,10 @@ class Enemy1(Enemy):
 
 class Enemy2(Enemy):
     pass
+
+
+
+class Bullet(Entity):
+    def draw(self, surface):
+        pygame.draw.line(surface, (255, 255, 0), (self.x, self.y),
+                         (self.x + self.width, self.y + self.height))
--- rogue.py
+++ rogue.py
@@ -12,15 +12,31 @@ x, y = 150, 150
 running = True
 clock = pygame.time.Clock()
 player = Player(200, 200, 50, 50)
+bullets = []
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            bullets.append(player.shot())
     screen.fill((0, 0, 0))
     player.update()
     player.draw(screen)
+
+    for bullet in bullets[:]:
+        bullet.update()
+        bullet.draw(screen)
+        if bullet.x > width:
+            bullets.remove(bullet)
+        if bullet.y > height:
+            bullets.remove(bullet)
+        if bullet.x + bullet.width < 0:
+            bullets.remove(bullet)
+        if bullet.y + bullet.height < 0:
+            bullets.remove(bullet)
+
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
