7fe5c92c8c73660b2fbc38bdae035724bbba82b8
dennaumol <dennaumol05@gmail.com>
2021-12-06 15:22:37 +0300

    jump and walk

--- entities.py
+++ entities.py
@@ -10,6 +10,8 @@ class Entity:
         self.move = list(move)
         self.animation_tick = 0
         self.animation_images = []
+        self.jump_count = 10
+        self.is_jump = False
 
     def update(self):
         self.animation_tick += 1
@@ -18,6 +20,18 @@ class Entity:
 
     def draw(self, surface):
         pass
+
+    def jump(self):
+        if self.is_jump:
+            if self.jump_count >= - 10:
+                if self.jump_count < 0:
+                    self.y += (self.jump_count ** 2) / 2
+                else:
+                    self.y -= (self.jump_count ** 2) / 2
+                self.jump_count -= 1
+            else:
+                self.jump_count = 10
+                self.is_jump = False
 
 
 class Player(Entity):
--- rogue.py
+++ rogue.py
@@ -2,7 +2,6 @@ import pygame
 
 from entities import Player
 
-
 pygame.init()
 size = width, height = 1024, 576
 screen = pygame.display.set_mode(size)
@@ -17,10 +16,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        player.move[0] = 7
+    if keys[pygame.K_a]:
+        player.move[0] = -7
+    if not keys[pygame.K_a] and not keys[pygame.K_d]:
+        player.move[0] = 0
+    if keys[pygame.K_SPACE]:
+        player.is_jump = True
+
+    if player.is_jump:
+        player.jump()
 
     screen.fill((0, 0, 0))
     player.update()
     player.draw(screen)
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
