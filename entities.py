21bebfc8a416964b3f0e99f9e503549f7d380d93
Zet992 <rodionovalexey11@mail.ru>
2022-01-15 23:56:08 +0300

    release v1.0.0

--- entities.py
+++ entities.py
@@ -404,7 +404,7 @@ class Player(Entity):
         self.shot_sound.play()
 
     def get_damage(self, damage):
-        self.hp -= damage - damage
+        self.hp -= damage
 
 
 class Enemy(Entity):
